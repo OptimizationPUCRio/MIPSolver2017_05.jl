2a056981a70c585b25e13807eba2f1444701ac8b
raphaelsaavedra <raphael.saavedra93@gmail.com>
2017-10-28 10:30:50 -0200

    Teste

--- branch_and_bound.jl
+++ branch_and_bound.jl
@@ -4,22 +4,19 @@ using JuMP, Gurobi
 ## Recebe o modelo JuMP de um problema binário puro.
 function branch(model::JuMP.Model)
 
-  opt = false
-  while opt == false
-    status = solve(model, relaxation=true)
-    isZero = model.colVal .== 0
-    isOne  = model.colVal .== 1
-    if sum(isZero + isOne) == model.numCols
-      # Solução relaxada é binária
-      opt = true
-    end
+  status = solve(model, relaxation=true)
+  isZero = model.colVal .== 0
+  isOne  = model.colVal .== 1
+  if sum(isZero + isOne) == model.numCols
+    # Solução relaxada é binária
+    opt = true
+    return true
   end
 
-  return true
-
+  return false
 end
 
 mutable struct node
   level::Int
-  problem::JuMP.Model
+  model::JuMP.Model
 end
