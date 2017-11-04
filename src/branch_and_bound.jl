61d18e6d01f69808ceefb05b7c6cf648f1e9ac9d
raphaelsaavedra <raphael.saavedra93@gmail.com>
2017-11-04 16:24:03 -0200

    Finishing branch part

--- src/branch_and_bound.jl
+++ src/branch_and_bound.jl
@@ -6,26 +6,27 @@ mutable struct node
   model::JuMP.Model
 end
 
-## Receives a pure binary JuMP model
-function branch_and_bound(model::JuMP.Model)
-
-  model.colCat[:] = :Cont
-  nodes = Vector{node}(1)
-  nodes[1] = node(0, model) # root
-
-  while !isempty(nodes) || currentNode.level <= log2(nvars)+1
-    solve(nodes[1].model)
-    if isBinary(nodes[1].model)
-
-    else
-      (leftChild, rightChild) = branch(nodes[1])
-      nodes = deleteat!(nodes, 1)
-      nodes = push!(nodes, leftChild)
-      nodes = push!(nodes, rightChild)
-    end
+## Checks if solution is binary
+function isBinary(model::JuMP.Model)
+  isZero = model.colVal .== 0
+  isOne  = model.colVal .== 1
+  if sum(isZero + isOne) == model.numCols
+    return true
   end
-
   return false
+end
+
+## Checks if model is max: if it is, converts to min
+function convertSense!(m::JuMP.Model)
+  model = copy(m)
+  if model.objSense == :Max
+    model.objSense = :Min
+    model.obj = -model.obj
+  else
+    model.objSense = :Max
+    model.obj = -model.obj
+  end
+  return model
 end
 
 ## Receives node and creates two children by setting a variable to 0 and 1 respectively
@@ -46,12 +47,50 @@ function branch(currentNode::node)
   return leftChild, rightChild
 end
 
-function isBinary(model::JuMP.Model)
-  isZero = model.colVal .== 0
-  isOne  = model.colVal .== 1
-  if sum(isZero + isOne) == model.numCols
-    # Relaxed solution is binary: optimal solution
-    return true
+## Receives a pure binary JuMP model
+function branch_and_bound(model::JuMP.Model)
+
+  # Check if model is max; if it is, converts to min
+  flagConverted = 0
+  if model.objSense == :Max
+    model = convertSense!(model)
+    flagConverted = 1
   end
-  return false
+
+  # Best lower bound: start as Inf
+  bestLB = Inf
+
+  # Relax all variables and create branch and bound tree
+  model.colCat[:] = :Cont
+  nodes = Vector{node}(1)
+  nodes[1] = node(0, model) # root
+
+  iter = 0
+  while !isempty(nodes)
+    status = solve(nodes[1].model)
+    if status == :Infeasible
+      # Relaxed problem is infeasible -- don't branch
+    elseif isBinary(nodes[1].model) && status == :Optimal
+      # Relaxed solution is binary: optimal solution -- don't branch
+      if nodes[1].model.objVal < bestLB
+        bestLB = nodes[1].model.objVal
+        optModel = copy(nodes[1].model)
+      end
+    else
+      # Relaxed solution is not optimal -- branch
+      (leftChild, rightChild) = branch(nodes[1])
+      nodes = push!(nodes, leftChild)
+      nodes = push!(nodes, rightChild)
+    end
+    nodes = deleteat!(nodes, 1)
+    iter+=1
+  end
+
+  # Check if model was converted; if it was, convert back to original sense
+  if flagConverted == 1
+    optModel = convertSense!(optModel)
+    solve(optModel)
+  end
+
+  return optModel
 end
