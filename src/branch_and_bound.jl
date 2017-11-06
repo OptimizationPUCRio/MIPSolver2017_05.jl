5833b03b53fca19b4d7b24bce1fc53e120c7a92c
raphaelsaavedra <raphael.saavedra93@gmail.com>
2017-11-06 12:57:33 -0200

    Adding binaryIndices in case problem is mixed

--- src/branch_and_bound.jl
+++ src/branch_and_bound.jl
@@ -18,7 +18,7 @@ end
 
 ## Checks if model is max: if it is, converts to min
 function convertSense!(m::JuMP.Model)
-  model = copy(m)
+  model = deepcopy(m)
   if model.objSense == :Max
     model.objSense = :Min
     model.obj = -model.obj
@@ -30,14 +30,15 @@ function convertSense!(m::JuMP.Model)
 end
 
 ## Receives node and creates two children by setting a variable to 0 and 1 respectively
-function branch(currentNode::node)
-  indToSet = indmax(currentNode.model.colUpper - currentNode.model.colLower)
+function branch(currentNode::node, binaryIndices::Vector{Int64})
+  # indToSet = indmax(currentNode.model.colUpper - currentNode.model.colLower)
+  indToSet = binaryIndices[level+1]
 
-  leftModel = copy(currentNode.model)
+  leftModel = deepcopy(currentNode.model)
   leftModel.colUpper[indToSet] = 0
   leftModel.colLower[indToSet] = 0
 
-  rightModel = copy(currentNode.model)
+  rightModel = deepcopy(currentNode.model)
   rightModel.colUpper[indToSet] = 1
   rightModel.colLower[indToSet] = 1
 
@@ -49,6 +50,8 @@ end
 
 ## Receives a pure binary JuMP model
 function branch_and_bound(model::JuMP.Model)
+
+  binaryIndices = find(model.colCat .== :Bin)
 
   # Check if model is max; if it is, converts to min
   flagConverted = 0
@@ -74,7 +77,7 @@ function branch_and_bound(model::JuMP.Model)
       # Relaxed solution is binary: optimal solution -- don't branch
       if nodes[1].model.objVal < bestLB
         bestLB = nodes[1].model.objVal
-        optModel = copy(nodes[1].model)
+        optModel = deepcopy(nodes[1].model)
       end
     else
       # Relaxed solution is not optimal -- branch
@@ -89,8 +92,12 @@ function branch_and_bound(model::JuMP.Model)
   # Check if model was converted; if it was, convert back to original sense
   if flagConverted == 1
     optModel = convertSense!(optModel)
-    solve(optModel)
+    status = solve(optModel)
+  else
+    status = solve(optModel)
   end
+
+  optModel.ext[:status] = status
 
   return optModel
 end
